537f365db131ade52070583d9fec6b77dbb4751b
huess <yildiz.h@eppendorf.de>
2023-12-27 12:13:19 +0100

    feat(day21): part one

--- day21/day21.go
+++ day21/day21.go
@@ -1,1 +1,172 @@
 package day21
+
+import (
+	"container/heap"
+	"fmt"
+	"math"
+
+	"github.com/fatih/color"
+)
+
+var primary = color.New(color.FgRed).SprintFunc()
+var secondary = color.New(color.FgYellow).SprintFunc()
+
+var directions = []int{
+	0, 1, 2, 3,
+}
+
+type Node struct {
+	next      []int
+	stepsLeft int
+}
+
+type PriorityQueue []*Node
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].stepsLeft < pq[j].stepsLeft
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+func (pq *PriorityQueue) Push(x any) {
+	item := x.(*Node)
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil
+	*pq = old[0 : n-1]
+	return item
+}
+
+func Travel(input []string, steps int) int {
+	start := []int{0, 0}
+	finalPlots := 0
+
+	for lI, l := range input {
+		for cI, c := range l {
+			if c == 'S' {
+				start = []int{lI, cI}
+			}
+		}
+	}
+
+	finalPlots = Walk(input, start, steps)
+
+	return finalPlots
+}
+
+func Walk(m []string, start []int, stepsLeft int) int {
+	var pq = PriorityQueue{
+		&Node{
+			next:      start,
+			stepsLeft: stepsLeft,
+		},
+	}
+
+	heap.Init(&pq)
+
+	visited := make(map[string]int)
+	total := 0
+	counter := make(map[string]int)
+
+	for pq.Len() > 0 {
+		node := heap.Pop(&pq).(*Node)
+		sl := node.stepsLeft - 1
+		nodeKey := fmt.Sprintf("%v|%v", node.next, sl)
+
+		if _, ok := visited[nodeKey]; ok {
+			continue
+		}
+
+		validNeighbours := 0
+
+		var directionToNext = map[int][]int{
+			0: {node.next[0] - 1, node.next[1]},
+			1: {node.next[0], node.next[1] + 1},
+			2: {node.next[0] + 1, node.next[1]},
+			3: {node.next[0], node.next[1] - 1},
+		}
+
+		for dirIndex := 0; dirIndex < len(directions); dirIndex++ {
+			next := directionToNext[directions[dirIndex]]
+			normNext := Normalize(next[0], next[1], len(m), len(m[0]))
+			nextNodeKey := fmt.Sprintf("%v", next)
+			nextChar := m[normNext[0]][normNext[1]]
+
+			if nextChar != '#' {
+				if sl == 0 {
+					if _, ok := counter[nextNodeKey]; !ok {
+						validNeighbours++
+						total++
+						counter[nextNodeKey] = 1
+					}
+				} else {
+					heap.Push(&pq, &Node{next: next, stepsLeft: sl})
+				}
+			}
+		}
+
+		visited[nodeKey] = validNeighbours
+	}
+
+	for lI, l := range m {
+		fmt.Print("\n")
+		for cI, c := range l {
+			nodeKey := fmt.Sprintf("%v", []int{lI, cI})
+			if _, ok := counter[nodeKey]; ok {
+				fmt.Print(primary(string("0")))
+			} else {
+				fmt.Print(secondary(string(c)))
+			}
+		}
+	}
+
+	fmt.Print("\n---------------\n")
+	return total
+}
+
+func Normalize(x, y, xLen, yLen int) []int {
+	rx, ry := x, y
+
+	if x >= xLen {
+		rx = x % xLen
+	}
+
+	if x < 0 {
+		if int(math.Abs(float64(x))) >= xLen {
+			if (x % xLen) == 0 {
+				rx = 0
+			} else {
+				rx = xLen + (x % xLen)
+			}
+		} else {
+			rx = xLen + x
+		}
+	}
+
+	if y >= yLen {
+		ry = y % yLen
+	}
+
+	if y < 0 {
+		if int(math.Abs(float64(y))) >= yLen {
+			if (y % yLen) == 0 {
+				ry = 0
+			} else {
+				ry = yLen + (y % yLen)
+			}
+		} else {
+			ry = yLen + y
+		}
+	}
+
+	return []int{rx, ry}
+}
--- day21/day21_test.go
+++ day21/day21_test.go
@@ -1,1 +1,180 @@
 package day21
+
+import (
+	"aoc2023/utils"
+	"os"
+	"strconv"
+	"testing"
+
+	"github.com/joho/godotenv"
+)
+
+var testInput = []string{
+	"...........",
+	".....###.#.",
+	".###.##..#.",
+	"..#.#...#..",
+	"....#.#....",
+	".##..S####.",
+	".##..#...#.",
+	".......##..",
+	".##.#.####.",
+	".##..##.##.",
+	"...........",
+}
+
+func TestTravel(t *testing.T) {
+	expected := 16
+	actual := Travel(testInput, 6)
+
+	if actual != expected {
+		t.Errorf("Expected %+v, Got %+v", expected, actual)
+	}
+}
+
+func TestTravelWithExpand(t *testing.T) {
+	expected := 50
+	actual := Travel(testInput, 10)
+
+	if actual != expected {
+		t.Errorf("Expected %+v, Got %+v", expected, actual)
+	}
+}
+
+func TestTravelWithExpand2(t *testing.T) {
+	expected := 1594
+	actual := Travel(testInput, 50)
+
+	if actual != expected {
+		t.Errorf("Expected %+v, Got %+v", expected, actual)
+	}
+}
+
+func TestTravelWithExpand3(t *testing.T) {
+	expected := 6536
+	actual := Travel(testInput, 100)
+
+	if actual != expected {
+		t.Errorf("Expected %+v, Got %+v", expected, actual)
+	}
+}
+
+// func TestTravelWithExpand4(t *testing.T) {
+// 	expected := 167004
+// 	actual := Travel(testInput, 500)
+
+// 	if actual != expected {
+// 		t.Errorf("Expected %+v, Got %+v", expected, actual)
+// 	}
+// }
+
+func TestTravelWithInput(t *testing.T) {
+	godotenv.Load()
+	expected, _ := strconv.Atoi(os.Getenv("result_1"))
+
+	Input, _ := utils.ReadInputFile("input.txt")
+	actual := Travel(Input, 64)
+
+	if actual != expected {
+		t.Errorf("Expected %+v, got %+v", expected, actual)
+	}
+}
+
+// func TestNormalize(t *testing.T) {
+// 	expected := []int{1, 1}
+// 	actual := Normalize(1, 1, 10, 10)
+
+// 	if expected[0] != actual[0] || expected[1] != actual[1] {
+// 		t.Errorf("Expected %+v, got %+v", expected, actual)
+// 	}
+// }
+
+// func TestNormalize00(t *testing.T) {
+// 	expected := []int{0, 0}
+// 	actual := Normalize(0, 0, 10, 10)
+
+// 	if expected[0] != actual[0] || expected[1] != actual[1] {
+// 		t.Errorf("Expected %+v, got %+v", expected, actual)
+// 	}
+// }
+
+// func TestNormalize0(t *testing.T) {
+// 	expected := []int{0, 0}
+// 	actual := Normalize(10, 10, 10, 10)
+
+// 	if expected[0] != actual[0] || expected[1] != actual[1] {
+// 		t.Errorf("Expected %+v, got %+v", expected, actual)
+// 	}
+// }
+
+// func TestNormalize1(t *testing.T) {
+// 	expected := []int{1, 1}
+// 	actual := Normalize(11, 11, 10, 10)
+
+// 	if expected[0] != actual[0] || expected[1] != actual[1] {
+// 		t.Errorf("Expected %+v, got %+v", expected, actual)
+// 	}
+// }
+
+// func TestNormalize2(t *testing.T) {
+// 	expected := []int{9, 9}
+// 	actual := Normalize(-11, -11, 10, 10)
+
+// 	if expected[0] != actual[0] || expected[1] != actual[1] {
+// 		t.Errorf("Expected %+v, got %+v", expected, actual)
+// 	}
+// }
+
+// func TestNormalize22(t *testing.T) {
+// 	expected := []int{0, 0}
+// 	actual := Normalize(-10, -10, 10, 10)
+
+// 	if expected[0] != actual[0] || expected[1] != actual[1] {
+// 		t.Errorf("Expected %+v, got %+v", expected, actual)
+// 	}
+// }
+
+// func TestNormalize3(t *testing.T) {
+// 	expected := []int{9, 2}
+// 	actual := Normalize(-1, -8, 10, 10)
+
+// 	if expected[0] != actual[0] || expected[1] != actual[1] {
+// 		t.Errorf("Expected %+v, got %+v", expected, actual)
+// 	}
+// }
+
+// func TestNormalize4(t *testing.T) {
+// 	expected := []int{4, 10}
+// 	actual := Normalize(4, -1, 11, 11)
+
+// 	if expected[0] != actual[0] || expected[1] != actual[1] {
+// 		t.Errorf("Expected %+v, got %+v", expected, actual)
+// 	}
+// }
+
+// func TestNormalize5(t *testing.T) {
+// 	expected := []int{0, 0}
+// 	actual := Normalize(-11, -11, 11, 11)
+
+// 	if expected[0] != actual[0] || expected[1] != actual[1] {
+// 		t.Errorf("Expected %+v, got %+v", expected, actual)
+// 	}
+// }
+
+// func TestNormalize6(t *testing.T) {
+// 	expected := []int{0, 0}
+// 	actual := Normalize(-22, -22, 11, 11)
+
+// 	if expected[0] != actual[0] || expected[1] != actual[1] {
+// 		t.Errorf("Expected %+v, got %+v", expected, actual)
+// 	}
+// }
+
+// func TestNormalize7(t *testing.T) {
+// 	expected := []int{1, 9}
+// 	actual := Normalize(-12, -10, 11, 11)
+
+// 	if expected[0] != actual[0] || expected[1] != actual[1] {
+// 		t.Errorf("Expected %+v, got %+v", expected, actual)
+// 	}
+// }
